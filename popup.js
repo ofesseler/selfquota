4f57d17e50e14747d92c980707c608d28e0b5d37
ofesseler <ofesseler@gmail.com>
2013-02-24 17:38:16 +0100

    passt balken an und verschönert ihn.
    zudem werden alle unnötigen abhängigkeiten zu flot und jquery gelöscht.

--- popup.js
+++ popup.js
@@ -1,67 +1,31 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 
 var req = new XMLHttpRequest();
 req.open(
     "GET",
     "http://www.selfnet.de/quota.xml",
     true);
-req.onload = showQuota;
+req.onload = css;
 req.send(null);
 
 $(document).ready(function () {
 });
 
-
-
-
-function showQuota() {
-  // asd();
-  $.plot($("#placeholder"), [ [[0, 0],[1,50],[2,125], [new Date().getDate(), 250]] ], options );
-
-    var options = {
-        series: { shadowSize: 0 }, // drawing is faster without shadows
-        yaxis: { min: 0, max: getTotal() },
-        xaxis: { min: 1, max: new Date().getDate() }
-    };
-  css();
-}
-
 function css() {
 
   var balken = document.createElement("div");
-  var style = document.createAttribute("style")
+  var style = document.createAttribute("style");
+  var style_id = document.createAttribute("id");
+  style_id.nodeValue="used";
+  balken.setAttributeNode(style_id);
   var used = req.responseXML.getElementsByTagName("used")[0];
-  style.nodeValue="height:100%; width:"+used.getAttribute("percentage")+"%; background:red;";
+  var total = req.responseXML.getElementsByTagName("total")[0];
+  style.nodeValue="height:100%; width:"+used.getAttribute("percentage")*100+"%;";
   balken.setAttributeNode(style);
-  balken.appendChild(document.createTextNode("Used: " + used.getAttribute("caption")))
+  var text = document.createTextNode(used.getAttribute("caption") + " / " + total.getAttribute("caption"));
+  
   document.getElementById("cssbalken").appendChild(balken);
+  document.getElementById("text").appendChild(text);
 }
-
-// function asd() {
-//   var total = req.responseXML.getElementsByTagName("total");
-//   var used = req.responseXML.getElementsByTagName("used");
-//   var availible = req.responseXML.getElementsByTagName("availible");
-//   var color = req.responseXML.getElementsByTagName("color");
-
-//   var useddiv = document.createElement("div");
-//   var usedText = document.createTextNode("Used: " + used[0].getAttribute("bytes") + " bytes");
-
-//   useddiv.appendChild(usedText);
-//   useddiv.appendChild(document.createTextNode(" Total: " + getTotal() + " GB"));
-//   document.body.appendChild(useddiv);
-
-
-
-//   // for (var i = 0, photo; photo = photos[i]; i++) {
-//   //   var img = document.createElement("image");
-//   //   img.src = constructImageURL(photo);
-//   //   document.body.appendChild(img);
-//   // }
-// }
-
 
 function getTotal() {
   var total = req.responseXML.getElementsByTagName("total");
